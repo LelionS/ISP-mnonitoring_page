83b3cc8c86871c5b2fc618bc50648cee6ec09a8d
Leli_n <122529284+LelionS@users.noreply.github.com>
2024-09-16 09:46:15 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -144,5 +144,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'liamleli44@gmail.com'
-EMAIL_HOST_PASSWORD = 'mdjkzogupynzbdim'
+EMAIL_HOST_USER = ' ' #enter your email
+EMAIL_HOST_PASSWORD = ' ' #enter your email host password
